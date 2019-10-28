cee520586274042f1e669ce7799e9d154111f21e
tripu <t@tripu.info>
2019-10-28 22:42:37 +0100

    Arrow keys now work on glyph pop-up

--- dist/firefox/js/glyphs.js
+++ dist/firefox/js/glyphs.js
@@ -1,4 +1,50 @@
-document.addEventListener('keydown', e => {
-    if ('Escape' === e.code || 'Enter' === e.code)
+// let sections = [];
+let items = [];
+let row;
+let col;
+
+const moveCursor = dir => {
+    const height = items.length;
+    const width = items[row].length;
+    items[row][col].classList.remove('highlight');
+    if ('ArrowRight' === dir) {
+        col = (col + 1) % width;
+    } else if ('ArrowLeft' === dir) {
+        col = (col + width - 1) % width;
+    } else if ('ArrowDown' === dir) {
+        row = (row + 1) % height;
+        col = 0;
+    } else if ('ArrowUp' === dir) {
+        row = (row + height - 1) % height;
+        col = 0;
+    }
+    items[row][col].classList.add('highlight');
+};
+
+const processKey = e => {
+    if ('Escape' === e.code)
         window.close();
-});
+    else if ('ArrowLeft' === e.code || 'ArrowRight' === e.code || 'ArrowUp' === e.code || 'ArrowDown' === e.code)
+        moveCursor(e.code);
+    else if ('Enter' === e.code) {
+        const glyph = items[row][col].innerText;
+        const copyEvent = new ClipboardEvent('copy', { dataType: 'text/plain', data: glyph });
+        document.dispatchEvent(copyEvent);
+        // window.close();
+    }
+};
+
+const build = () => {
+    const sections = document.getElementsByTagName('section');
+    for (let i of sections) {
+        items.push(i.getElementsByTagName('div'));
+    }
+    row = col = 0;
+};
+
+const init = () => {
+    build();
+    document.addEventListener('keydown', processKey);
+};
+
+window.addEventListener('load', init);
